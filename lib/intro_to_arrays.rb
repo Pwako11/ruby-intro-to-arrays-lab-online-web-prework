a7aa58be1384ccd9070bc760d8c088a8c9c70485
Patrick A <akolo@hotmail.com>
2019-03-03 18:19:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,27 +1,27 @@
 def instantiate_new_array
-	my_new_array = []
-	size = my_new_array.size
+	@my_new_array = []
+	size = @my_new_array.size
 end 
 
 def array_with_two_elements
-	my_new_array = ["cars", "bikes"]
-	size = my_new_array.size
+	@my_new_array = ["cars", "bikes"]
+	size = @my_new_array.size
 end 
 
 def taylor_swift 
-	taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  my_first_element = taylor_swift.first
-  my_third_element = taylor_swift.third
-	my_last_element = taylor_swift.last
+	@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_first_element = @taylor_swift.first
+  my_third_element = @taylor_swift.third
+	my_last_element = @taylor_swift.last
 end 
 
 def south_east_asia  
-	south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-	my_last_element = south_east_asia.last
-	first_country = south_east_asia.first
+	@south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+	my_last_element = @south_east_asia.last
+	first_country = @south_east_asia.first
 end
 
 def programming_languages  
-	programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
-	length = programming_languages.size
+	@programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+	length = @programming_languages.size
 end
