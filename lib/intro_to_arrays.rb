096fd73d5c2aebf8dfb379f6607787686e730368
Patrick A <akolo@hotmail.com>
2019-03-03 17:45:04 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,65 @@
+def instantiate_new_array
+	@my_new_array = []
+	puts @my_new_array.inspect
+end 
+
+def instantiate_new_array
+  @my_new_array = []
+	size = @my_new_array.size
+end 
+
+def array_with_two_elements
+	@my_new_array = ["cars", "bikes"]
+	puts @my_new_array.inspect
+	size = @my_new_array.size
+end 
+
+def taylor_swift 
+	@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+end 
+
+def taylor_swift 
+	@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+	my_first_element = @taylor_swift.first
+end
+
+def taylor_swift 
+	@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+	my_third_element = @taylor_swift.third
+end
+
+
+def taylor_swift 
+	@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+	my_last_element = @taylor_swift.last
+end
+
+
+def south_east_asia  
+	@south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+	my_last_element = @south_east_asia.last
+end
+
+def south_east_asia  
+	@south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+	first_country = @south_east_asia.first
+end
+
+def south_east_asia  
+	@south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+	first_country = @south_east_asia.first
+end
+
+def south_east_asia  
+	@south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+	last_country = @south_east_asia.last
+end
+
+def programming_languages  
+	@programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+end
+
+def programming_languages  
+	@programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+	length = @programming_languages.size
+end
