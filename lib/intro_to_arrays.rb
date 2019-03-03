325bd6b3478a26185f1ee3394ff2e62a9d7f512c
Patrick A <akolo@hotmail.com>
2019-03-03 18:41:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -9,6 +9,7 @@ end
 def indexing  
 	@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
 	end 
+	
 def first_element
 	@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   @taylor_swift[0]
