d7d743f0d647b4a6f33a0bf57304928495e08738
Patrick A <akolo@hotmail.com>
2019-03-03 18:06:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,10 +1,8 @@
 def instantiate_new_array
 	my_new_array = []
-end 
-
-def instantiate_new_array
-  my_new_array = []
 	size = my_new_array.size
+end 
+	
 end 
 
 def array_with_two_elements
@@ -14,48 +12,16 @@ end
 
 def taylor_swift 
 	taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-end 
-
-def taylor_swift 
-	taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-	my_first_element = taylor_swift.first
-end
-
-def taylor_swift 
-	taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-	my_third_element = taylor_swift.third
-end
-
-
-def taylor_swift 
-	taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_first_element = taylor_swift.first
+  my_third_element = taylor_swift.third
 	my_last_element = taylor_swift.last
-end
-
+end 
 
 def south_east_asia  
 	south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
 	my_last_element = south_east_asia.last
-end
-
-def south_east_asia  
-	south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
 	first_country = south_east_asia.first
-end
 
-def south_east_asia  
-	south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-	first_country = south_east_asia.first
-end
-
-def south_east_asia  
-	south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-	last_country = south_east_asia.last
-end
-
-def programming_languages  
-	programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
-end
 
 def programming_languages  
 	programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
